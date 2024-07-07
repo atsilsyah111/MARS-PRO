4bf844e7e3b2137c12cf440f109bcb29e3a8fe33
atsilsyah111 <atsilsyah1@gmail.com>
2024-07-07 10:39:53 +0700

    Add files via upload

---
+++ Dept.jsx
@@ -0,0 +1,285 @@
+import React from "react";
+import logofix from "../assets/logofix.png";
+import { useState } from "react";
+
+const Dept = () => {
+  const [nama_agen_kapal, setNamaAgenKapal] = useState("");
+  const [perusahaan_agen_kapal, setPerusahaanAgenKapal] = useState("");
+  const [imo_number, setIMONumber] = useState("");
+  const [nama_kapal, setNamaKapal] = useState("");
+  const [kebangsaan_kapal, setKebangsaanKapal] = useState("");
+  const [data_cru_indonesia, setDataCRUIndonesia] = useState("");
+  const [data_cru_asing, setDataCRUAsing] = useState("");
+  const [pelabuhan_asal, setPelabuhanAsal] = useState("");
+  const [pelabuhan_tujuan, setPelabuhanTujuan] = useState("");
+  const [service_location, setServiceLocation] = useState("");
+  const [jadwal_keberangkatan, setJadwalKeberangkatan] = useState("");
+  const [tujuan_keberangkatan, setTujuanKeberangkatan] = useState("");
+  const [dokument, setDokument] = useState("");
+  const handleDataKeberangkatan = async (e) => {
+    e.preventDefault();
+    const response = await fetch(
+      "http://localhost:3001/api/createBerangkatKapal",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          nama_agen_kapal,
+          perusahaan_agen_kapal,
+          imo_number,
+          nama_kapal,
+          kebangsaan_kapal,
+          data_cru_indonesia,
+          data_cru_asing,
+          pelabuhan_asal,
+          pelabuhan_tujuan,
+          service_location,
+          jadwal_keberangkatan,
+          tujuan_keberangkatan,
+          dokument,
+        }),
+      }
+    );
+    if (response.ok) {
+      alert("berhasil");
+    } else {
+      alert("failed");
+    }
+  };
+  return (
+    <div>
+      <div className="px-[50px] py-[50px]">
+        <div className="flex justify-between items-center absolute gap-[100px]">
+          <div>
+            <a className="" href="/">
+              <img className="w-[113px] h-[101px]" src={logofix} alt="" />
+            </a>
+          </div>
+          <div className="px-[100px]">
+            <ul className="flex justify-between gap-[185px] text-[24px] font-bold">
+              <li>
+                <a className="" href="/arrival">
+                  Form Kedatangan
+                </a>
+              </li>
+              <li>
+                <a className="text-[#1A719C]" href="/departure">
+                  Form Keberangkatan
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div className="text-[48px] font-bold flex pt-[166px] justify-center gap-[10px]">
+            <div>FORM</div>
+            <div className="text-[#1A719C]">KEBERANGKATAN</div>
+            <div>KAPAL</div>
+          </div>
+          <div className="flex justify-center font-bold text-[20px] text-[#5C5C68]">
+            <div className="pt-[11px]">Silahkan isi data dibawah</div>
+          </div>
+        </div>
+        <div className="pt-[48px] flex justify-center">
+          <div className="flex font-bold text-left text-[20px]">
+            <form
+              className="flex justify-between items-end gap-[54px]"
+              onSubmit={handleDataKeberangkatan}
+            >
+              <div className="grid gap-[30px]">
+                <div>
+                  <div className="pl-[17px]">Nama Agen Kapal</div>
+                  <div className="pt-[11px]">
+                    <input
+                      type="text"
+                      value={nama_agen_kapal}
+                      onChange={(e) => setNamaAgenKapal(e.target.value)}
+                      placeholder="Masukkan nama Agen Kapal"
+                      className="pl-[17px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px]"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="pl-[17px]">Perusahaan Agen Kapal</div>
+                  <div className="pt-[11px]">
+                    <input
+                      type="text"
+                      value={perusahaan_agen_kapal}
+                      onChange={(e) => setPerusahaanAgenKapal(e.target.value)}
+                      placeholder="Masukkan Perusahaan Agen Kapal"
+                      className="pl-[17px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px]"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="pl-[17px]">IMO Number</div>
+                  <div className="pt-[11px]">
+                    <input
+                      type="text"
+                      value={imo_number}
+                      onChange={(e) => setIMONumber(e.target.value)}
+                      placeholder="Masukkan IMO Number"
+                      className="pl-[17px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px]"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="pl-[17px]">Nama Kapal</div>
+                  <div className="pt-[11px]">
+                    <input
+                      type="text"
+                      value={nama_kapal}
+                      onChange={(e) => setNamaKapal(e.target.value)}
+                      placeholder="Masukkan Nama Kapal"
+                      className="pl-[17px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px]"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="pl-[17px]">Kebangsaan Kapal</div>
+                  <div className="pt-[11px]">
+                    <input
+                      type="text"
+                      value={kebangsaan_kapal}
+                      onChange={(e) => setKebangsaanKapal(e.target.value)}
+                      placeholder="Masukkan Kebangsaan Kapal"
+                      className="pl-[17px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px]"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="pl-[17px]">Dara Kru Indonesia</div>
+                  <div className="pt-[11px]">
+                    <input
+                      type="text"
+                      value={data_cru_indonesia}
+                      onChange={(e) => setDataCRUIndonesia(e.target.value)}
+                      placeholder="Masukkan Data Kru Indonesia"
+                      className="pl-[17px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px]"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="pl-[17px]">Data Kru Asing</div>
+                  <div className="pt-[11px]">
+                    <input
+                      type="text"
+                      value={data_cru_asing}
+                      onChange={(e) => setDataCRUAsing(e.target.value)}
+                      placeholder="Masukkan Data Crew Asing"
+                      className="pl-[17px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px]"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="pl-[17px]">Pelabuhan Asal Kapal</div>
+                  <div className="pt-[11px]">
+                    <input
+                      type="text"
+                      value={pelabuhan_asal}
+                      onChange={(e) => setPelabuhanAsal(e.target.value)}
+                      placeholder="Masukkan Pelabuhan Asal Kapal"
+                      className="pl-[17px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px]"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="pl-[17px]">Pelabuhan Tujuan Kapal</div>
+                  <div className="pt-[11px]">
+                    <input
+                      type="text"
+                      value={pelabuhan_tujuan}
+                      onChange={(e) => setPelabuhanTujuan(e.target.value)}
+                      placeholder="Masukkan Pelabuhan Tujuan Kapal"
+                      className="pl-[17px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px]"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="pl-[17px]">Lokasi Pelayanan</div>
+                  <div className="pt-[11px] relative">
+                    <select
+                      className="pl-[17px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px] appearance-none"
+                      value={service_location}
+                      onChange={(e) => setServiceLocation(e.target.value)}
+                    >
+                      <option value="" disabled hidden>
+                        Masukkan Lokasi Pelayanan
+                      </option>
+                      <option value="Kantor Imigrasi Kelas II TPI Lhokseumawe">
+                        Kantor Imigrasi Kelas II TPI Lhokseumawe
+                      </option>
+                      <option value="Kantor Imigrasi Kelas II TPI Sabang">
+                        Kantor Imigrasi Kelas II TPI Sabang
+                      </option>
+                      <option value="Kantor Imigrasi Kelas I TPI Banda Aceh">
+                        Kantor Imigrasi Kelas I TPI Banda Aceh
+                      </option>
+                    </select>
+                  </div>
+                </div>
+                <div>
+                  <div className="pl-[17px]">Jadwal Keberangkatan Kapal</div>
+                  <div className="pt-[11px] relative">
+                    <input
+                      type="date"
+                      value={jadwal_keberangkatan}
+                      placeholder="Pilih Jadwal Kedatangan Kapal"
+                      onChange={(e) => setJadwalKeberangkatan(e.target.value)}
+                      className="pl-[17px] pr-[20px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px] appearance-none"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="pl-[17px]">Tujuan Keberangkatan</div>
+                  <div className="pt-[11px] relative">
+                    <select
+                      className="pl-[17px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px] appearance-none"
+                      value={tujuan_keberangkatan}
+                      onChange={(e) => setTujuanKeberangkatan(e.target.value)}
+                    >
+                      <option>Pilih Tujuan Keberangkatan</option>
+                      <option value="Medical Evacuation">
+                        Crew Change
+                      </option>
+                      <option value="Clearance">Clearance Document</option>
+                      <option value="Docking">Docking</option>
+                    </select>
+                  </div>
+                </div>
+                <div>
+                  <div className="pl-[17px]">Upload Dokumen (link)</div>
+                  <div className="pt-[11px]">
+                    <input
+                      value={dokument}
+                      onChange={(e) => setDokument(e.target.value)}
+                      placeholder="Silahkan Upload Dokumen"
+                      className="pl-[17px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px]"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="w-[214px] h-[65px] bg-[#1A719C] rounded-[20px] text-white"
+                >
+                  Submit Data Kapal
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="pt-[200px]">
+        <div className="footer h-[100px] bg-[#1A719C] flex justify-center place-items-center font-bold text-[20px] text-white">
+          Integrated of Banda Aceh, Sabang, and Lhokseumawe Information System
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dept;
---
+++ SignOff.jsx
@@ -0,0 +1,211 @@
+import React from "react";
+import logofix from "../assets/logofix.png";
+import { useState } from "react";
+
+const SignOff = () => {
+  const [nama_cru, setNamaCru] = useState("");
+  const [no_paspor, setNoPaspor] = useState("");
+  const [kebangsaan_cru, setKebangsaanCRU] = useState("");
+  const [tg_rencana_sign_off, setRencanaOff] = useState("");
+  const [nama_kapal, setNamaKapal] = useState("");
+  const [kebangsaan_kapal, setKebangsaanKapal] = useState("");
+  const [surat, setSurat] = useState("");
+  const [waktu_lapor, setWaktuLapor] = useState("");
+  const [nama_agen, setNamaAgen] = useState("");
+
+  const handleDataSignOff = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:3001/api/createSignOffCru", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        nama_cru,
+        no_paspor,
+        kebangsaan_cru,
+        tg_rencana_sign_off,
+        nama_kapal,
+        kebangsaan_kapal,
+        surat,
+        waktu_lapor,
+        nama_agen,
+      }),
+    });
+    if (response.ok) {
+      alert("berhasil");
+    } else {
+      alert("failed");
+    }
+  };
+  return (
+    <div>
+      <div className="px-[50px] py-[50px]">
+        <div className="flex justify-between items-center absolute gap-[224px]">
+          <div>
+            <a className="" href="/">
+              <img className="w-[113px] h-[101px]" src={logofix} alt="" />
+            </a>
+          </div>
+          <div>
+            <ul className="flex justify-between gap-[185px] text-[24px] font-bold">
+              <li>
+                <a className="" href="/sign-on">
+                  Sign-On Form
+                </a>
+              </li>
+              <li>
+                <a className="text-[#1A719C]" href="/sign-off">
+                  Sign-Off Form
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div className="text-[48px] font-bold flex pt-[166px] justify-center gap-[10px]">
+            <div>Kru</div>
+            <div className="text-[#1A719C]">Sign-Off</div>
+            <div>Data Form</div>
+          </div>
+          <div className="flex justify-center font-bold text-[20px] text-[#5C5C68]">
+            <div className="pt-[11px]">Fill in your crew details</div>
+          </div>
+        </div>
+        <div className="pt-[48px] flex justify-center">
+          <div className="flex font-bold text-left text-[20px]">
+            <form
+              className="flex justify-between items-end gap-[54px]"
+              onSubmit={handleDataSignOff}
+            >
+              <div className="grid gap-[30px]">
+                <div>
+                  <div className="pl-[17px]">Nama Kru</div>
+                  <div className="pt-[11px]">
+                    <input
+                      type="text"
+                      value={nama_cru}
+                      onChange={(e) => setNamaCru(e.target.value)}
+                      placeholder="Input Nama Crew"
+                      className="pl-[17px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px]"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="pl-[17px]">Nomor Paspor</div>
+                  <div className="pt-[11px]">
+                    <input
+                      type="text"
+                      value={no_paspor}
+                      onChange={(e) => setNoPaspor(e.target.value)}
+                      placeholder="Input Nomor Passport"
+                      className="pl-[17px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px]"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="pl-[17px]">Kebangsaan Kru</div>
+                  <div className="pt-[11px]">
+                    <input
+                      type="text"
+                      value={kebangsaan_cru}
+                      onChange={(e) => setKebangsaanCRU(e.target.value)}
+                      placeholder="Input Kebangsaan Crew"
+                      className="pl-[17px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px]"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="pl-[17px]">Jadwal Sign On</div>
+                  <div className="pt-[11px]">
+                    <input
+                      type="date"
+                      value={tg_rencana_sign_off}
+                      onChange={(e) => setRencanaOff(e.target.value)}
+                      className="pl-[17px] pr-[20px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px] appearance-none"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="pl-[17px]">Nama Kapal</div>
+                  <div className="pt-[11px]">
+                    <input
+                      type="text"
+                      value={nama_kapal}
+                      onChange={(e) => setNamaKapal(e.target.value)}
+                      placeholder="Input Nama Kapal"
+                      className="pl-[17px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px]"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="pl-[17px]">Kebangsaan Kapal</div>
+                  <div className="pt-[11px]">
+                    <input
+                      type="text"
+                      value={kebangsaan_kapal}
+                      onChange={(e) => setKebangsaanKapal(e.target.value)}
+                      placeholder="Input Kebangsaan Kapal"
+                      className="pl-[17px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px]"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="pl-[17px]">Upload Dokumen</div>
+                  <div className="pt-[11px]">
+                    <input
+                      type="text"
+                      value={surat}
+                      onChange={(e) => setSurat(e.target.value)}
+                      placeholder="Upload Dokumen"
+                      className="pl-[17px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px]"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="pl-[17px]">Waktu Melapor</div>
+                  <div className="pt-[11px]">
+                    <input
+                      type="date"
+                      value={waktu_lapor}
+                      placeholder="Input Waktu Melapor"
+                      onChange={(e) => setWaktuLapor(e.target.value)}
+                      className="pl-[17px] pr-[20px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px] appearance-none"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="pl-[17px]">Nama Agen</div>
+                  <div className="pt-[11px]">
+                    <input
+                      type="text"
+                      value={nama_agen}
+                      onChange={(e) => setNamaAgen(e.target.value)}
+                      placeholder="Input Nama Agen"
+                      className="pl-[17px] bg-[#83B3CA] placeholder-[#5C5C68] w-[594px] h-[65px] rounded-[20px]"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="w-[214px] h-[65px] bg-[#1A719C] rounded-[20px] text-white"
+                >
+                  Buat Data Kru
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="pt-[200px]">
+        <div className="footer h-[100px] bg-[#1A719C] flex justify-center place-items-center font-bold text-[20px] text-white">
+          Integrated of Banda Aceh, Sabang, and Lhokseumawe Information System
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignOff;
